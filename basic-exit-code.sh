059f198ca7173dc13e643649d3a35b1325edec39
KarenG <krngzmn10@gmail.com>
2024-09-24 10:53:19 -0400

    adding class notes from 9-24-2024

---
+++ basic-exit-code.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#echo out a command that works, then echo out the exit code
+date
+echo $?
+echo "----"
---
+++ this-or-that.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "This is my code running. " && echo "This is more of my code"
+
+echo "This is some more code" || exit 1
+
+echo "This is some more code || exit 99
